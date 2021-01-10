b538fe0d46a96bf21628a1d61d897bc1d9c518df
keno <keno123@126.com>
2021-01-10 20:25:01 +0800

    add function sample:timeout/timestamp

--- app/src/main/java/kiscode/fake/rxjava/demo/real/fragment/RxJavaFunctionFragment.java
+++ app/src/main/java/kiscode/fake/rxjava/demo/real/fragment/RxJavaFunctionFragment.java
@@ -16,18 +16,24 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.schedulers.Timed;
 import kiscode.fake.rxjava.demo.R;
 
 /**
  * Description: 功能函数
- *  1. delay 延迟x秒发射
- *  2. doOnNext/doOnComplete/doOnError 在下游执行之前回调该方法
- *  3. doAfterNext在 下游onNext执行之后回调该方法
+ * 1. delay 延迟x秒发射
+ * 2. doOnNext/doOnComplete/doOnError/doOnSubscribe 在下游执行之前回调该方法
+ * 3. doAfterNext在 下游onNext执行之后回调该方法
+ * 4. timeout 上游超过指定时长没有发射，会发送一个错误通知；timeout变体可以向下发射定制内容或切换线程
+ * 5. timestamp 它将一个发射T类型数据的Observable转换为一个发射类型为Timestamped<T>的数据的Observable，每一项都包含数据的原始发射时间
  * Author: keno
  * CreateDate: 2021/1/6 22:35
  */
@@ -61,6 +67,8 @@ public class RxJavaFunctionFragment extends Fragment implements View.OnClickList
         view.findViewById(R.id.btn_rxjava_delay).setOnClickListener(this);
         view.findViewById(R.id.btn_rxjava_doOnNext).setOnClickListener(this);
         view.findViewById(R.id.btn_rxjava_doAfterNext).setOnClickListener(this);
+        view.findViewById(R.id.btn_rxjava_timeout).setOnClickListener(this);
+        view.findViewById(R.id.btn_rxjava_timestamp).setOnClickListener(this);
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -79,6 +87,12 @@ public class RxJavaFunctionFragment extends Fragment implements View.OnClickList
                 break;
             case R.id.btn_rxjava_doAfterNext:
                 useDoAfterNext();
+                break;
+            case R.id.btn_rxjava_timeout:
+                useTimeout();
+                break;
+            case R.id.btn_rxjava_timestamp:
+                useTimeStamp();
                 break;
         }
     }
@@ -109,6 +123,13 @@ public class RxJavaFunctionFragment extends Fragment implements View.OnClickList
                 emitter.onNext(2);
                 emitter.onNext(3);
                 emitter.onComplete();
+            }
+        }).doOnSubscribe(new Consumer<Disposable>() {
+            @Override
+            public void accept(Disposable disposable) throws Exception {
+                //在onSubscribe 之前执行
+                Log.i(TAG, "accept: doOnSubscribe");
+
             }
         }).doOnNext(new Consumer<Integer>() {
             @Override
@@ -148,7 +169,6 @@ public class RxJavaFunctionFragment extends Fragment implements View.OnClickList
         });
 
 
-
     }
 
     private void useDoAfterNext() {
@@ -163,7 +183,7 @@ public class RxJavaFunctionFragment extends Fragment implements View.OnClickList
         }).doAfterNext(new Consumer<Integer>() {
             @Override
             public void accept(Integer integer) throws Exception {
-                Log.i(TAG,"doAfterNext accept :"+integer);
+                Log.i(TAG, "doAfterNext accept :" + integer);
             }
         }).subscribe(new Observer<Integer>() {
             @Override
@@ -187,5 +207,94 @@ public class RxJavaFunctionFragment extends Fragment implements View.OnClickList
             }
         });
     }
+
+
+    /***
+     * timeout 上游超过指定时长没有发射，会发送一个错误通知；timeout变体可以向下发射定制内容或切换线程
+     */
+    private void useTimeout() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception {
+                for (int i = 1; i < 10; i++) {
+                    int sleepTime = i == 5 ? 5000 : 500;
+                    Thread.sleep(sleepTime);
+                    emitter.onNext(i);
+                }
+                emitter.onComplete();
+            }
+        })/*.timeout(3000, TimeUnit.MILLISECONDS)  //上游如果指定时长没有发射则 抛出异常*/
+                .timeout(3000, TimeUnit.MILLISECONDS, new ObservableSource<Integer>() {
+                    @Override
+                    public void subscribe(@NonNull Observer<? super Integer> observer) {
+                        Log.i(TAG, "timeout subscribe");
+                        observer.onNext(909);  //
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        Log.i(TAG, "onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Integer value) {
+                        Log.i(TAG, "onNext:" + value);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.e(TAG, "onError:" + e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "onComplete");
+                    }
+                });
+    }
+
+
+    /***
+     * timestamp 它将一个发射T类型数据的Observable转换为一个发射类型为Timestamped<T>的数据的Observable，每一项都包含数据的原始发射时间
+     */
+    private void useTimeStamp() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception {
+                for (int i = 1; i < 100; i++) {
+                    Thread.sleep(i);
+                    emitter.onNext(i);
+                }
+                emitter.onComplete();
+            }
+        }).timestamp()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Timed<Integer>>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        Log.i(TAG, "onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Timed<Integer> integerTimed) {
+                        Log.i(TAG, "onNext:" + "time:" + integerTimed.time() + "\tvalue：" + integerTimed.value());
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.e(TAG, "onError:" + e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "onComplete");
+                    }
+                });
+    }
+
 
 }
